1ed8589156e9bbb6c6a5b6df18a17d9d579195f9
preeti9667 <saini04preeti@gmail.com>
2025-09-11 15:59:58 +0530

    add netlify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body  cz-shortcut-listen="true"
+  >
         {children}
       </body>
     </html>
